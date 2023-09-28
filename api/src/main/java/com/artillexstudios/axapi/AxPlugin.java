a6cfbf4f021bba3719216947de74a0020588c2f5
TomTom <93038247+AverageGithub@users.noreply.github.com>
2023-09-28 18:37:17 +0200

    remove pluginlib cause it isn't working and I don't want to mess with it rn

--- api/src/main/java/com/artillexstudios/axapi/AxPlugin.java
+++ api/src/main/java/com/artillexstudios/axapi/AxPlugin.java
@@ -8,10 +8,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
-import pluginlib.DependentJavaPlugin;
 
-public abstract class AxPlugin extends DependentJavaPlugin {
+public abstract class AxPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
