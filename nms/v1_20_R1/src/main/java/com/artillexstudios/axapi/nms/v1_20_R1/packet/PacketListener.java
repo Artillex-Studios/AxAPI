f04e5cd8c2318105ed6220140c5341e917c078fb
TomTom <93038247+AverageGithub@users.noreply.github.com>
2023-09-29 20:17:26 +0200

    The issue is somewhere here!

--- nms/v1_20_R1/src/main/java/com/artillexstudios/axapi/nms/v1_20_R1/packet/PacketListener.java
+++ nms/v1_20_R1/src/main/java/com/artillexstudios/axapi/nms/v1_20_R1/packet/PacketListener.java
@@ -6,6 +6,7 @@ import com.artillexstudios.axapi.events.PacketEntityInteractEvent;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPromise;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.protocol.game.ServerboundInteractPacket;
 import net.minecraft.world.InteractionHand;
@@ -24,30 +25,33 @@ public class PacketListener extends ChannelDuplexHandler {
 
     @Override
     public void channelRead(@NotNull ChannelHandlerContext ctx, @NotNull Object msg) throws Exception {
+        System.out.println("Channel read! " + msg.getClass());
         if (msg instanceof ServerboundInteractPacket packet) {
             try {
                 FriendlyByteBuf byteBuf = new FriendlyByteBuf(Unpooled.buffer());
                 packet.write(byteBuf);
+                System.out.println(byteBuf);
 
-                int entityId = byteBuf.readVarInt();
-                ServerboundInteractPacket.ActionType type = byteBuf.readEnum(ServerboundInteractPacket.ActionType.class);
-                InteractionHand hand = null;
-                Vector vector = null;
-                boolean attack = false;
-                if (type == ServerboundInteractPacket.ActionType.INTERACT) {
-                    hand = byteBuf.readEnum(InteractionHand.class);
-                } else if (type == ServerboundInteractPacket.ActionType.INTERACT_AT) {
-                    vector = new Vector(byteBuf.readFloat(), byteBuf.readFloat(), byteBuf.readFloat());
-                    hand = byteBuf.readEnum(InteractionHand.class);
-                } else {
-                    attack = true;
-                }
-                byteBuf.clear();
-
-                PacketEntity entity = PacketEntityTracker.getById(entityId);
-                if (entity != null) {
-                    Bukkit.getPluginManager().callEvent(new PacketEntityInteractEvent(player, entity, attack, vector, hand == InteractionHand.MAIN_HAND ? EquipmentSlot.HAND : EquipmentSlot.OFF_HAND));
-                }
+//                int entityId = byteBuf.readVarInt();
+//                ServerboundInteractPacket.ActionType type = byteBuf.readEnum(ServerboundInteractPacket.ActionType.class);
+//                InteractionHand hand = null;
+//                Vector vector = null;
+//                boolean attack = false;
+//                if (type == ServerboundInteractPacket.ActionType.INTERACT) {
+//                    hand = byteBuf.readEnum(InteractionHand.class);
+//                } else if (type == ServerboundInteractPacket.ActionType.INTERACT_AT) {
+//                    vector = new Vector(byteBuf.readFloat(), byteBuf.readFloat(), byteBuf.readFloat());
+//                    hand = byteBuf.readEnum(InteractionHand.class);
+//                } else {
+//                    attack = true;
+//                }
+//                byteBuf.clear();
+//
+//                PacketEntity entity = PacketEntityTracker.getById(entityId);
+//                if (entity != null) {
+//                    Bukkit.getPluginManager().callEvent(new PacketEntityInteractEvent(player, entity, attack, vector, hand == InteractionHand.MAIN_HAND ? EquipmentSlot.HAND : EquipmentSlot.OFF_HAND));
+//                }
+                System.out.println("Interact packet");
             } catch (Exception exception) {
                 exception.printStackTrace();
             }
