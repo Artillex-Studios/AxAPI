f1defa5b65460e7eeec5ce7558db62133e145339
TomTom <93038247+AverageGithub@users.noreply.github.com>
2023-09-30 10:21:51 +0200

    Optimize packet entity tracking

--- api/src/main/java/com/artillexstudios/axapi/entity/PacketEntityTracker.java
+++ api/src/main/java/com/artillexstudios/axapi/entity/PacketEntityTracker.java
@@ -1,28 +1,31 @@
 package com.artillexstudios.axapi.entity;
 
 import com.artillexstudios.axapi.entity.impl.PacketEntity;
-import it.unimi.dsi.fastutil.ints.Int2ObjectAVLTreeMap;
-import it.unimi.dsi.fastutil.ints.Int2ObjectArrayMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectLinkedOpenHashMap;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
-
-import java.util.ArrayList;
-import java.util.concurrent.ConcurrentHashMap;
+import org.bukkit.util.NumberConversions;
 
 public class PacketEntityTracker {
-    private static final Int2ObjectAVLTreeMap<PacketEntity> entities = new Int2ObjectAVLTreeMap<>();
+    private static final Int2ObjectLinkedOpenHashMap<PacketEntity> entities = new Int2ObjectLinkedOpenHashMap<>();
     private static final Object mutex = new Object();
 
     public static void tickAll() {
         synchronized (mutex) {
+            Location playerLocation = new Location(null, 0, 0, 0, 0, 0);
             for (Player player : Bukkit.getOnlinePlayers()) {
-                for (PacketEntity entity : entities.values()) {
-                    if (!entity.getLocation().getWorld().getUID().equals(player.getLocation().getWorld().getUID())) {
+                // Reuse the same location object with the values changed, so we can avoid unnecessary heap churn
+                player.getLocation(playerLocation);
+
+                entities.values().forEach(entity -> {
+                    if (!entity.getLocation().getWorld().equals(playerLocation.getWorld())) {
                         entity.hide(player);
-                        continue;
+                        return;
                     }
 
-                    if (player.getLocation().distanceSquared(entity.getLocation()) <= entity.getViewDistanceSquared()) {
+                    // We can use a dumbed-down version of Bukkit's distanceSquared, as we know that the worlds are the same
+                    if (distanceSquared(playerLocation, entity.getLocation()) <= entity.getViewDistanceSquared()) {
                         try {
                             entity.show(player);
                         } catch (Exception exception) {
@@ -31,9 +34,13 @@ public class PacketEntityTracker {
                     } else {
                         entity.hide(player);
                     }
-                }
+                });
             }
         }
+    }
+
+    public static double distanceSquared(Location location, Location other) {
+        return NumberConversions.square(location.getX() - other.getX()) + NumberConversions.square(location.getY() - other.getY()) + NumberConversions.square(location.getZ() - other.getZ());
     }
 
     public static void startTracking(PacketEntity entity) {
