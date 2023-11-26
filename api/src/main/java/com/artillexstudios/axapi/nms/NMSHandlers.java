e26b68825fda1d2cd055beb8693d7b408b5253f0
AverageGithub <adorjantoto09@gmail.com>
2023-11-26 15:44:35 +0100

    Fix NMS stuff (no idea why it wasn't like this)

--- api/src/main/java/com/artillexstudios/axapi/nms/NMSHandlers.java
+++ api/src/main/java/com/artillexstudios/axapi/nms/NMSHandlers.java
@@ -4,6 +4,7 @@ import com.artillexstudios.axapi.entity.PacketEntityFactory;
 import com.artillexstudios.axapi.hologram.HologramFactory;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import com.artillexstudios.axapi.utils.Version;
 
 public class NMSHandlers {
     private static PacketEntityFactory packetEntityFactory;
@@ -12,12 +13,11 @@ public class NMSHandlers {
     private static String version;
 
     public static boolean initialise(JavaPlugin plugin) {
-        final String packageName = Bukkit.getServer().getClass().getPackage().getName();
-        version = packageName.substring(packageName.lastIndexOf('.') + 1);
+        Version version = Version.getServerVersion();
 
         if (packetEntityFactory == null) {
             try {
-                packetEntityFactory = (PacketEntityFactory) Class.forName(String.format("com.artillexstudios.axapi.nms.%s.PacketEntityFactory", version)).getConstructor().newInstance();
+                packetEntityFactory = (PacketEntityFactory) Class.forName(String.format("com.artillexstudios.axapi.nms.%s.PacketEntityFactory", version.nmsVersion)).getConstructor().newInstance();
             } catch (Exception exception) {
                 return false;
             }
@@ -25,7 +25,7 @@ public class NMSHandlers {
 
         if (hologramFactory == null) {
             try {
-                hologramFactory = (HologramFactory) Class.forName(String.format("com.artillexstudios.axapi.nms.%s.HologramFactory", version)).getConstructor().newInstance();
+                hologramFactory = (HologramFactory) Class.forName(String.format("com.artillexstudios.axapi.nms.%s.HologramFactory", version.nmsVersion)).getConstructor().newInstance();
             } catch (Exception exception) {
                 return false;
             }
@@ -33,7 +33,7 @@ public class NMSHandlers {
 
         if (nmsHandler == null) {
             try {
-                nmsHandler = (NMSHandler) Class.forName(String.format("com.artillexstudios.axapi.nms.%s.NMSHandler", version)).getConstructor(JavaPlugin.class).newInstance(plugin);
+                nmsHandler = (NMSHandler) Class.forName(String.format("com.artillexstudios.axapi.nms.%s.NMSHandler", version.nmsVersion)).getConstructor(JavaPlugin.class).newInstance(plugin);
             } catch (Exception exception) {
                 return false;
             }
