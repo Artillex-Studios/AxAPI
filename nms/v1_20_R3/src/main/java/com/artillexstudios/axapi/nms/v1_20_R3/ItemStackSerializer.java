ccdedf68999f3652eaa62ed808f83ae0b5658cc7
AverageGithub <adorjantoto09@gmail.com>
2023-12-07 18:19:17 +0100

    Add 1.20.3 support

---
+++ nms/v1_20_R3/src/main/java/com/artillexstudios/axapi/nms/v1_20_R3/ItemStackSerializer.java
@@ -0,0 +1,34 @@
+package com.artillexstudios.axapi.nms.v1_20_R3;
+
+import com.google.common.base.Preconditions;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtIo;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_20_R3.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
+import net.minecraft.nbt.NbtAccounter;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+public class ItemStackSerializer {
+
+    public byte[] serializeAsBytes(ItemStack itemStack) {
+        Preconditions.checkArgument(itemStack != null, "Can't serialise a null itemstack!");
+        Preconditions.checkArgument(itemStack.getType() != Material.AIR, "Can't serialise air!");
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            NbtIo.writeCompressed(CraftItemStack.asNMSCopy(itemStack).save(new CompoundTag()), outputStream);
+            return outputStream.toByteArray();
+        } catch (Exception exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    public ItemStack deserializeFromBytes(byte[] bytes) {
+        try (ByteArrayInputStream stream = new ByteArrayInputStream(bytes)) {
+            return CraftItemStack.asBukkitCopy(net.minecraft.world.item.ItemStack.of(NbtIo.readCompressed(stream, NbtAccounter.unlimitedHeap())));
+        } catch (Exception exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+}
