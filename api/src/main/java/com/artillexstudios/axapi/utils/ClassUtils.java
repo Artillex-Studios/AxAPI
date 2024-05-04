d8b74ada72a05cf53cbbc7b1820b086a9db3c309
TomTom <93038247+AverageGithub@users.noreply.github.com>
2024-05-04 11:09:36 +0200

    Add cache to classExists

--- api/src/main/java/com/artillexstudios/axapi/utils/ClassUtils.java
+++ api/src/main/java/com/artillexstudios/axapi/utils/ClassUtils.java
@@ -1,23 +1,29 @@
 package com.artillexstudios.axapi.utils;
 
+import com.artillexstudios.axapi.collections.ThreadSafeList;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sun.misc.Unsafe;
+
+import java.util.HashMap;
 
 public enum ClassUtils {
     INSTANCE;
 
     private final Logger log = LoggerFactory.getLogger(ClassUtils.class);
     private final Unsafe unsafe = UnsafeUtils.INSTANCE.unsafe();
+    private final HashMap<String, Boolean> CLASS_CACHE = new HashMap<>();
 
     public boolean classExists(@NotNull String className) {
-        try {
-            Class.forName(className);
-            return true;
-        } catch (Exception exception) {
-            return false;
-        }
+        return CLASS_CACHE.computeIfAbsent(className, name -> {
+            try {
+                Class.forName(name, false, this.getClass().getClassLoader());
+                return true;
+            } catch (Exception exception) {
+                return false;
+            }
+        });
     }
 
     public <T> T newInstance(String clazz) {
