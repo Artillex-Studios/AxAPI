3d8cc2a2ee6f6223314b8b2c19fece854e224787
TomTom <93038247+AverageGithub@users.noreply.github.com>
2024-09-14 18:01:48 +0200

    Whoopsies

--- api/src/main/java/com/artillexstudios/axapi/commands/arguments/annotation/Word.java
+++ api/src/main/java/com/artillexstudios/axapi/commands/arguments/annotation/Word.java
@@ -6,6 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
+@Target({ElementType.PARAMETER, ElementType.TYPE_PARAMETER})
 public @interface Word {
 }
