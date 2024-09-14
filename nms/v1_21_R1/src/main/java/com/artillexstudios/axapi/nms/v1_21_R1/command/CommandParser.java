34e6f2caa9f23f1cbc1302e1ce7c09f34a912c7a
TomTom <93038247+AverageGithub@users.noreply.github.com>
2024-09-14 16:48:00 +0200

    More fixes

--- nms/v1_21_R1/src/main/java/com/artillexstudios/axapi/nms/v1_21_R1/command/CommandParser.java
+++ nms/v1_21_R1/src/main/java/com/artillexstudios/axapi/nms/v1_21_R1/command/CommandParser.java
@@ -131,7 +131,7 @@ public class CommandParser {
                     counter++;
                 }
 
-                literal.then(l);
+                literal = literal.then(l);
             }
         }
 
