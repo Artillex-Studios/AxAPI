d5fe19d3819e7385121d0351dc9aa834bc653934
TomTom <93038247+AverageGithub@users.noreply.github.com>
2024-10-21 15:04:21 +0200

    Fix something that someone broke with CraftMagicNumbers

--- nms/v1_21_R1/src/main/java/com/artillexstudios/axapi/nms/v1_21_R1/ItemStackSerializer.java
+++ nms/v1_21_R1/src/main/java/com/artillexstudios/axapi/nms/v1_21_R1/ItemStackSerializer.java
@@ -2,6 +2,7 @@ package com.artillexstudios.axapi.nms.v1_21_R1;
 
 import com.google.common.base.Preconditions;
 import com.mojang.serialization.Dynamic;
+import net.minecraft.SharedConstants;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtAccounter;
 import net.minecraft.nbt.NbtIo;
@@ -10,7 +11,6 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.datafix.fixes.References;
 import org.bukkit.Material;
 import org.bukkit.craftbukkit.inventory.CraftItemStack;
-import org.bukkit.craftbukkit.util.CraftMagicNumbers;
 import org.bukkit.inventory.ItemStack;
 
 import java.io.ByteArrayInputStream;
@@ -24,7 +24,7 @@ public enum ItemStackSerializer {
         Preconditions.checkArgument(itemStack.getType() != Material.AIR, "Can't serialise air!");
         try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
             CompoundTag compoundTag = (CompoundTag) CraftItemStack.asNMSCopy(itemStack).save(MinecraftServer.getServer().registryAccess());
-            compoundTag.putInt("DataVersion", CraftMagicNumbers.INSTANCE.getDataVersion());
+            compoundTag.putInt("DataVersion", SharedConstants.getCurrentVersion().getDataVersion().getVersion());
             NbtIo.writeCompressed(compoundTag, outputStream);
             return outputStream.toByteArray();
         } catch (Exception exception) {
@@ -36,7 +36,7 @@ public enum ItemStackSerializer {
         try (ByteArrayInputStream stream = new ByteArrayInputStream(bytes)) {
             CompoundTag compound = NbtIo.readCompressed(stream, NbtAccounter.unlimitedHeap());
             int dataVersion = compound.getInt("DataVersion");
-            CompoundTag converted = (CompoundTag) MinecraftServer.getServer().fixerUpper.update(References.ITEM_STACK, new Dynamic<>(NbtOps.INSTANCE, compound), dataVersion, CraftMagicNumbers.INSTANCE.getDataVersion()).getValue();
+            CompoundTag converted = (CompoundTag) MinecraftServer.getServer().fixerUpper.update(References.ITEM_STACK, new Dynamic<>(NbtOps.INSTANCE, compound), dataVersion, SharedConstants.getCurrentVersion().getDataVersion().getVersion()).getValue();
             return CraftItemStack.asCraftMirror(net.minecraft.world.item.ItemStack.parse(MinecraftServer.getServer().registryAccess(), converted).orElseThrow());
         } catch (Exception exception) {
             throw new RuntimeException(exception);
