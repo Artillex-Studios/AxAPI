45083cd905804021b58b9eff62e1cd1bdd54b650
TomTom <93038247+AverageGithub@users.noreply.github.com>
2024-11-02 10:03:47 +0100

    Fix packetentities on 1.21.3

--- nms/v1_21_R2/src/main/java/com/artillexstudios/axapi/nms/v1_21_R2/packet/ClientboundTeleportEntityWrapper.java
+++ nms/v1_21_R2/src/main/java/com/artillexstudios/axapi/nms/v1_21_R2/packet/ClientboundTeleportEntityWrapper.java
@@ -1,30 +1,16 @@
 package com.artillexstudios.axapi.nms.v1_21_R2.packet;
 
 import com.artillexstudios.axapi.packetentity.PacketEntity;
-import com.artillexstudios.axapi.reflection.ClassUtils;
-import com.artillexstudios.axapi.reflection.FastFieldAccessor;
 import net.minecraft.network.protocol.game.ClientboundTeleportEntityPacket;
+import net.minecraft.world.entity.PositionMoveRotation;
+import net.minecraft.world.phys.Vec3;
 import org.bukkit.Location;
 
+import java.util.Set;
+
 public class ClientboundTeleportEntityWrapper {
-    private static final FastFieldAccessor entityId = FastFieldAccessor.forClassField(ClientboundTeleportEntityPacket.class, "b");
-    private static final FastFieldAccessor x = FastFieldAccessor.forClassField(ClientboundTeleportEntityPacket.class, "c");
-    private static final FastFieldAccessor y = FastFieldAccessor.forClassField(ClientboundTeleportEntityPacket.class, "d");
-    private static final FastFieldAccessor z = FastFieldAccessor.forClassField(ClientboundTeleportEntityPacket.class, "e");
-    private static final FastFieldAccessor yRot = FastFieldAccessor.forClassField(ClientboundTeleportEntityPacket.class, "f");
-    private static final FastFieldAccessor xRot = FastFieldAccessor.forClassField(ClientboundTeleportEntityPacket.class, "g");
-    private static final FastFieldAccessor onGround = FastFieldAccessor.forClassField(ClientboundTeleportEntityPacket.class, "h");
 
     public static ClientboundTeleportEntityPacket createNew(PacketEntity entity, Location location) {
-        ClientboundTeleportEntityPacket packet = ClassUtils.INSTANCE.newInstance(ClientboundTeleportEntityPacket.class);
-        entityId.setInt(packet, entity.id());
-        x.setDouble(packet, location.getX());
-        y.setDouble(packet, location.getY());
-        z.setDouble(packet, location.getZ());
-        yRot.setByte(packet, (byte) ((int) (location.getYaw() * 256.0F / 360.0F)));
-        xRot.setByte(packet, (byte) ((int) (location.getPitch() * 256.0F / 360.0F)));
-        onGround.setBoolean(packet, true);
-
-        return packet;
+        return ClientboundTeleportEntityPacket.teleport(entity.id(), new PositionMoveRotation(new Vec3(location.getX(), location.getY(), location.getZ()), Vec3.ZERO, location.getYaw(), location.getPitch() * 256.0F / 360.0F), Set.of(), true);
     }
 }
