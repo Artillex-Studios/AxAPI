6f23641082def89f08fd52b6960cb306a7619442
TomTom <93038247+AverageGithub@users.noreply.github.com>
2025-01-01 17:56:18 +0100

    Fix inconsistency with cooldown, should have no issues now!

--- api/src/main/java/com/artillexstudios/axapi/utils/Cooldown.java
+++ api/src/main/java/com/artillexstudios/axapi/utils/Cooldown.java
@@ -4,53 +4,64 @@ import it.unimi.dsi.fastutil.objects.Object2LongArrayMap;
 import it.unimi.dsi.fastutil.objects.Object2LongMap;
 import it.unimi.dsi.fastutil.objects.ObjectIterator;
 
+import java.time.Duration;
+
 public class Cooldown<T> {
     private final Object2LongArrayMap<T> cooldowns = new Object2LongArrayMap<>();
-    private long minCooldown = Long.MAX_VALUE;
-    private long lastCheck = 0;
+    private long closestTime = Long.MAX_VALUE;
 
     public void addCooldown(T key, long time) {
-        doHouseKeeping();
+        this.doHouseKeeping();
 
-        minCooldown = Math.min(minCooldown, time);
+        long closest = System.currentTimeMillis() + time;
+        this.closestTime = Math.min(this.closestTime, closest);
 
-        cooldowns.put(key, System.currentTimeMillis() + time);
+        this.cooldowns.put(key, closest);
+    }
+
+    public void addCooldown(T key, Duration duration) {
+        this.doHouseKeeping();
+
+        long closest = System.currentTimeMillis() + duration.toMillis();
+        this.closestTime = Math.min(this.closestTime, closest);
+
+        this.cooldowns.put(key, closest);
     }
 
     public long getRemaining(T key) {
-        doHouseKeeping();
+        this.doHouseKeeping();
 
-        return cooldowns.getOrDefault(key, System.currentTimeMillis()) - System.currentTimeMillis();
+        return this.cooldowns.getOrDefault(key, System.currentTimeMillis()) - System.currentTimeMillis();
     }
 
     public long getRemainingAsSeconds(T key) {
-        doHouseKeeping();
+        this.doHouseKeeping();
 
-        return (cooldowns.getOrDefault(key, System.currentTimeMillis()) - System.currentTimeMillis()) / 1000;
+        return (this.cooldowns.getOrDefault(key, System.currentTimeMillis()) - System.currentTimeMillis()) / 1000;
     }
 
     public boolean hasCooldown(T key) {
-        doHouseKeeping();
-        return cooldowns.containsKey(key);
+        this.doHouseKeeping();
+        return this.cooldowns.containsKey(key);
     }
 
     public void clear() {
-        cooldowns.clear();
+        this.cooldowns.clear();
     }
 
     public void remove(T key) {
-        doHouseKeeping();
+        this.doHouseKeeping();
 
-        cooldowns.removeLong(key);
+        this.cooldowns.removeLong(key);
     }
 
     private void doHouseKeeping() {
-        if (System.currentTimeMillis() - lastCheck < minCooldown) {
+        if (System.currentTimeMillis() < this.closestTime) {
             return;
         }
 
         ObjectIterator<Object2LongMap.Entry<T>> iterator = cooldowns.object2LongEntrySet().iterator();
-        long time = lastCheck = System.currentTimeMillis();
+        long time = System.currentTimeMillis();
 
         while (iterator.hasNext()) {
             Object2LongMap.Entry<T> entry = iterator.next();
