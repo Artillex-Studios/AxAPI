e858b3b614d4b7d384b9245c939ba5dfd008f5e0
TomTom <93038247+AverageGithub@users.noreply.github.com>
2025-01-01 17:51:56 +0100

    Better description for MetricsConfig

--- api/src/main/java/com/artillexstudios/axapi/metrics/MetricsConfig.java
+++ api/src/main/java/com/artillexstudios/axapi/metrics/MetricsConfig.java
@@ -5,12 +5,14 @@ import com.artillexstudios.axapi.config.annotation.ConfigurationPart;
 
 import java.util.UUID;
 
-public class MetricsConfig implements ConfigurationPart {
+public final class MetricsConfig implements ConfigurationPart {
     @Comment("""
             If sending anonymous data should be enabled.
             Having this enabled helps us gain insightful data
             such as the server version, the java version you are using,
             and how popular our plugins are.
+            
+            Want to know more? Check out our metrics at: https://metrics.artillex-studios.com/
             
             We'd appreciate if you kept this enabled, as this has no impact on your performance.
             """)
