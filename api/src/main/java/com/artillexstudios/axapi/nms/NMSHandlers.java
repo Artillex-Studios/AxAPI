5b7eae1424754475cffdc121eb68d6e089173b6d
TomTom <93038247+AverageGithub@users.noreply.github.com>
2025-02-02 18:51:01 +0100

    Fix an issue in NMS

--- api/src/main/java/com/artillexstudios/axapi/nms/NMSHandlers.java
+++ api/src/main/java/com/artillexstudios/axapi/nms/NMSHandlers.java
@@ -1,5 +1,6 @@
 package com.artillexstudios.axapi.nms;
 
+import com.artillexstudios.axapi.AxPlugin;
 import com.artillexstudios.axapi.utils.LogUtils;
 import com.artillexstudios.axapi.utils.Version;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -8,7 +9,7 @@ public class NMSHandlers {
     private static NMSHandler nmsHandler;
 
     // Do not mix with innit, bruv!
-    private static boolean init(JavaPlugin plugin) {
+    private static boolean init(AxPlugin plugin) {
         Version version = Version.getServerVersion();
         if (version == Version.UNKNOWN) {
             LogUtils.warn("Could not load plugin {} due to version mismatch! Found protocol version {} which is unsupported!", plugin.getName(), Version.protocolVersion());
@@ -21,7 +22,7 @@ public class NMSHandlers {
         }
 
         try {
-            nmsHandler = (NMSHandler) Class.forName("com.artillexstudios.axapi.nms.%s.NMSHandler".formatted(version.nmsVersion)).getConstructor(JavaPlugin.class).newInstance(plugin);
+            nmsHandler = (NMSHandler) Class.forName("com.artillexstudios.axapi.nms.%s.NMSHandler".formatted(version.nmsVersion)).getConstructor(AxPlugin.class).newInstance(plugin);
         } catch (Exception exception) {
             LogUtils.warn("Could not enable NMSHandler due to an internal exception while loading NMSHandler!", exception);
             return false;
@@ -36,14 +37,14 @@ public class NMSHandlers {
 
     public static class British {
 
-        public static boolean initialise(JavaPlugin plugin) {
+        public static boolean initialise(AxPlugin plugin) {
             return init(plugin);
         }
     }
 
     public static class American {
 
-        public static boolean initialize(JavaPlugin plugin) {
+        public static boolean initialize(AxPlugin plugin) {
             return init(plugin);
         }
     }
