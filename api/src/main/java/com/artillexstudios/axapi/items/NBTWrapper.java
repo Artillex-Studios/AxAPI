fd373eb51bff6a3e30bd571f3372b4cbc28f75fc
TomTom <93038247+AverageGithub@users.noreply.github.com>
2025-03-15 16:39:20 +0100

    NBTWrapper work

--- api/src/main/java/com/artillexstudios/axapi/items/NBTWrapper.java
+++ api/src/main/java/com/artillexstudios/axapi/items/NBTWrapper.java
@@ -2,9 +2,12 @@ package com.artillexstudios.axapi.items;
 
 import com.artillexstudios.axapi.items.component.DataComponents;
 import com.artillexstudios.axapi.items.nbt.CompoundTag;
+import com.artillexstudios.axapi.items.nbt.ListTag;
 import com.artillexstudios.axapi.items.nbt.Tag;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 public class NBTWrapper {
@@ -86,6 +89,110 @@ public class NBTWrapper {
     public NBTWrapper set(String key, long[] value) {
         this.tag.putLongArray(key, value);
         return this;
+    }
+
+    public Byte getByte(String key) {
+        if (!this.tag.contains(key)) {
+            return null;
+        }
+
+        return this.tag.getByte(key);
+    }
+
+    public Short getShort(String key) {
+        if (!this.tag.contains(key)) {
+            return null;
+        }
+
+        return this.tag.getShort(key);
+    }
+
+    public Integer getInt(String key) {
+        if (!this.tag.contains(key)) {
+            return null;
+        }
+
+        return this.tag.getInt(key);
+    }
+
+    public Long getLong(String key) {
+        if (!this.tag.contains(key)) {
+            return null;
+        }
+
+        return this.tag.getLong(key);
+    }
+
+    public String getString(String key) {
+        if (!this.tag.contains(key)) {
+            return null;
+        }
+
+        return this.tag.getString(key);
+    }
+
+    public byte[] getByteArray(String key) {
+        if (!this.tag.contains(key)) {
+            return null;
+        }
+
+        return this.tag.getByteArray(key);
+    }
+
+    public long[] getLongArray(String key) {
+        if (!this.tag.contains(key)) {
+            return null;
+        }
+
+        return this.tag.getLongArray(key);
+    }
+
+    public int[] getIntArray(String key) {
+        if (!this.tag.contains(key)) {
+            return null;
+        }
+
+        return this.tag.getIntArray(key);
+    }
+
+    public Float getFloat(String key) {
+        if (!this.tag.contains(key)) {
+            return null;
+        }
+
+        return this.tag.getFloat(key);
+    }
+
+    public Double getDouble(String key) {
+        if (!this.tag.contains(key)) {
+            return null;
+        }
+
+        return this.tag.getDouble(key);
+    }
+
+    public UUID getUUID(String key) {
+        return this.tag.getUUID(key);
+    }
+
+    public ListTag getList(String key) {
+        return this.tag.getList(key);
+    }
+
+    public CompoundTag getCompound(String key) {
+        return this.tag.getCompound(key);
+    }
+
+    public boolean contains(String key) {
+        return this.tag.contains(key);
+    }
+
+    public boolean containsUUID(String key) {
+        return this.tag.containsUUID(key);
+    }
+
+    public Set<String> getAllKeys() {
+        return this.tag.getAllKeys();
     }
 
     public void build() {
