ee98608328cf31337abe38bb99c682ecc8286059
MachineBreaker <saltspigotpp@gmail.com>
2025-03-22 06:04:31 -0400

    Fix UniverseSpigot support

--- nms/v1_21_R1/src/main/java/com/artillexstudios/axapi/nms/v1_21_R1/utils/IBlockDataListCopier.java
+++ nms/v1_21_R1/src/main/java/com/artillexstudios/axapi/nms/v1_21_R1/utils/IBlockDataListCopier.java
@@ -3,9 +3,11 @@ package com.artillexstudios.axapi.nms.v1_21_R1.utils;
 import ca.spottedleaf.moonrise.common.list.IBlockDataList;
 import com.artillexstudios.axapi.reflection.ClassUtils;
 import com.artillexstudios.axapi.reflection.FastFieldAccessor;
+import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
 import it.unimi.dsi.fastutil.shorts.Short2LongOpenHashMap;
 
 import java.util.Arrays;
+import java.util.Map;
 
 public class IBlockDataListCopier {
     private static final FastFieldAccessor map = FastFieldAccessor.forClassField(IBlockDataList.class, "map");
@@ -14,7 +16,8 @@ public class IBlockDataListCopier {
 
     public static IBlockDataList copy(IBlockDataList list) {
         IBlockDataList newList = ClassUtils.INSTANCE.newInstance(IBlockDataList.class);
-        map.set(newList, ((Short2LongOpenHashMap) map.get(list)).clone());
+        Map mapField = map.get(list);
+        map.set(newList, mapField instanceof Short2LongOpenHashMap ? ((Short2LongOpenHashMap) mapField) : ((Int2IntOpenHashMap) mapField).clone());
         long[] index = byIndex.get(list);
         byIndex.set(newList, Arrays.copyOf(index, index.length));
         size.setInt(newList, size.getInt(list));
