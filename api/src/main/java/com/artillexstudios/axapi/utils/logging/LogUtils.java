d3d6e461e289a9574bb21e1e82c0d8f17cfa2fa6
TomTom <93038247+AverageGithub@users.noreply.github.com>
2025-03-28 17:53:24 +0100

    Fixes to LogUtils debugs, correctly show caller class

--- api/src/main/java/com/artillexstudios/axapi/utils/logging/LogUtils.java
+++ api/src/main/java/com/artillexstudios/axapi/utils/logging/LogUtils.java
@@ -1,80 +1,101 @@
 package com.artillexstudios.axapi.utils.logging;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class LogUtils {
     private static final StackWalker STACK_WALKER = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
-    private static final FileLogger logger = new FileLogger("debug-logs");
-
-    public static void debug(String message) {
-        debug(message, DebugMode.ALL);
-    }
+    private static final FileLogger fileLogger = new FileLogger("debug-logs");
 
     public static void debug(String message, DebugMode mode) {
+        debug(LoggerFactory.getLogger(STACK_WALKER.getCallerClass()), message, mode);
+    }
+
+    public static void debug(String message) {
+        debug(LoggerFactory.getLogger(STACK_WALKER.getCallerClass()), message, DebugMode.ALL);
+    }
+
+    private static void debug(Logger logger, String message, DebugMode mode) {
         if (mode == DebugMode.CONSOLE || mode == DebugMode.ALL) {
-            LoggerFactory.getLogger(STACK_WALKER.getCallerClass()).info(message);
+            logger.info(message);
         }
 
         if (mode == DebugMode.FILE || mode == DebugMode.ALL) {
-            logger.log(message);
+            fileLogger.log(message);
         }
     }
 
-    public static void debug(String message, Object object) {
-        debug(message, object, DebugMode.ALL);
+    public static void debug(String message, Object object, DebugMode mode) {
+        debug(LoggerFactory.getLogger(STACK_WALKER.getCallerClass()), message, object, mode);
     }
 
-    public static void debug(String message, Object object, DebugMode mode) {
+    public static void debug(String message, Object object) {
+        debug(LoggerFactory.getLogger(STACK_WALKER.getCallerClass()), message, object, DebugMode.ALL);
+    }
+
+    private static void debug(Logger logger, String message, Object object, DebugMode mode) {
         if (mode == DebugMode.CONSOLE || mode == DebugMode.ALL) {
-            LoggerFactory.getLogger(STACK_WALKER.getCallerClass()).info(message, object);
+            logger.info(message, object);
         }
 
         if (mode == DebugMode.FILE || mode == DebugMode.ALL) {
             String formatted = org.apache.commons.lang3.StringUtils.replace(message, "{}", object == null ? "null" : object.toString(), 1);
-            logger.log(formatted);
+            fileLogger.log(formatted);
         }
     }
 
-    public static void debug(String message, Object object, Object object2) {
-        debug(message, object, object2, DebugMode.ALL);
+    public static void debug(String message, Object object, Object object2, DebugMode mode) {
+        debug(LoggerFactory.getLogger(STACK_WALKER.getCallerClass()), message, object, object2, mode);
     }
 
-    public static void debug(String message, Object object, Object object2, DebugMode mode) {
+    public static void debug(String message, Object object, Object object2) {
+        debug(LoggerFactory.getLogger(STACK_WALKER.getCallerClass()), message, object, object2, DebugMode.ALL);
+    }
+
+    private static void debug(Logger logger, String message, Object object, Object object2, DebugMode mode) {
         if (mode == DebugMode.CONSOLE || mode == DebugMode.ALL) {
-            LoggerFactory.getLogger(STACK_WALKER.getCallerClass()).info(message, object, object2);
+            logger.info(message, object, object2);
         }
 
         if (mode == DebugMode.FILE || mode == DebugMode.ALL) {
             String formatted = org.apache.commons.lang3.StringUtils.replace(message, "{}", object == null ? "null" : object.toString(), 1);
             formatted = org.apache.commons.lang3.StringUtils.replace(formatted, "{}", object2 == null ? "null" : object2.toString(), 1);
-            logger.log(formatted);
+            fileLogger.log(formatted);
         }
     }
 
     public static void debug(String message, Object object, Object object2, Object object3) {
-        debug(message, object, object2, object3, DebugMode.ALL);
+        debug(LoggerFactory.getLogger(STACK_WALKER.getCallerClass()), message, object, object2, object3, DebugMode.ALL);
     }
 
     public static void debug(String message, Object object, Object object2, Object object3, DebugMode mode) {
+        debug(LoggerFactory.getLogger(STACK_WALKER.getCallerClass()), message, object, object2, object3, mode);
+    }
+
+    private static void debug(Logger logger, String message, Object object, Object object2, Object object3, DebugMode mode) {
         if (mode == DebugMode.CONSOLE || mode == DebugMode.ALL) {
-            LoggerFactory.getLogger(STACK_WALKER.getCallerClass()).info(message, object, object2, object3);
+            logger.info(message, object, object2, object3);
         }
 
         if (mode == DebugMode.FILE || mode == DebugMode.ALL) {
             String formatted = org.apache.commons.lang3.StringUtils.replace(message, "{}", object == null ? "null" : object.toString(), 1);
             formatted = org.apache.commons.lang3.StringUtils.replace(formatted, "{}", object2 == null ? "null" : object2.toString(), 1);
             formatted = org.apache.commons.lang3.StringUtils.replace(formatted, "{}", object3 == null ? "null" : object3.toString(), 1);
-            logger.log(formatted);
+            fileLogger.log(formatted);
         }
     }
 
-    public static void debug(String message, Object object, Object object2, Object object3, Object objec4) {
-        debug(message, object, object2, object3, objec4, DebugMode.ALL);
+    public static void debug(String message, Object object, Object object2, Object object3, Object object4) {
+        debug(LoggerFactory.getLogger(STACK_WALKER.getCallerClass()), message, object, object2, object3, object4, DebugMode.ALL);
     }
 
     public static void debug(String message, Object object, Object object2, Object object3, Object object4, DebugMode mode) {
+        debug(LoggerFactory.getLogger(STACK_WALKER.getCallerClass()), message, object, object2, object3, object4, mode);
+    }
+
+    private static void debug(Logger logger, String message, Object object, Object object2, Object object3, Object object4, DebugMode mode) {
         if (mode == DebugMode.CONSOLE || mode == DebugMode.ALL) {
-            LoggerFactory.getLogger(STACK_WALKER.getCallerClass()).info(message, object, object2, object3, object4);
+            logger.info(message, object, object2, object3, object4);
         }
 
         if (mode == DebugMode.FILE || mode == DebugMode.ALL) {
@@ -82,17 +103,21 @@ public final class LogUtils {
             formatted = org.apache.commons.lang3.StringUtils.replace(formatted, "{}", object2 == null ? "null" : object2.toString(), 1);
             formatted = org.apache.commons.lang3.StringUtils.replace(formatted, "{}", object3 == null ? "null" : object3.toString(), 1);
             formatted = org.apache.commons.lang3.StringUtils.replace(formatted, "{}", object4 == null ? "null" : object4.toString(), 1);
-            logger.log(formatted);
+            fileLogger.log(formatted);
         }
     }
 
-    public static void debug(String message, Object... arguments) {
-        debug(message, arguments, DebugMode.ALL);
+    public static void debug(String message, DebugMode mode, Object... arguments) {
+        debug(LoggerFactory.getLogger(STACK_WALKER.getCallerClass()), message, mode, arguments);
     }
 
-    public static void debug(String message, DebugMode mode, Object... arguments) {
+    public static void debug(String message, Object... arguments) {
+        debug(LoggerFactory.getLogger(STACK_WALKER.getCallerClass()), message, DebugMode.ALL, arguments);
+    }
+
+    private static void debug(Logger logger, String message, DebugMode mode, Object... arguments) {
         if (mode == DebugMode.CONSOLE || mode == DebugMode.ALL) {
-            LoggerFactory.getLogger(STACK_WALKER.getCallerClass()).info(message, arguments);
+            logger.info(message, arguments);
         }
 
         if (mode == DebugMode.FILE || mode == DebugMode.ALL) {
@@ -100,7 +125,7 @@ public final class LogUtils {
             for (Object argument : arguments) {
                 formatted = org.apache.commons.lang3.StringUtils.replace(formatted, "{}", argument == null ? "null" : argument.toString(), 1);
             }
-            logger.log(formatted);
+            fileLogger.log(formatted);
         }
     }
 
