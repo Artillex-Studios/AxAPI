cb4a23bf30a6c0192f73da47cdd71578c69feccd
TomTom <93038247+AverageGithub@users.noreply.github.com>
2025-03-30 16:23:18 +0200

    Revert "Try moving to a different system for packets for better performance"
    This reverts commit 3781e7b7fd1060bd29c984e7f8ac5dafcc85ffa9.

--- api/src/main/java/com/artillexstudios/axapi/AxPlugin.java
+++ api/src/main/java/com/artillexstudios/axapi/AxPlugin.java
@@ -23,6 +23,7 @@ import com.artillexstudios.axapi.placeholders.Placeholders;
 import com.artillexstudios.axapi.scheduler.Scheduler;
 import com.artillexstudios.axapi.utils.ComponentSerializer;
 import com.artillexstudios.axapi.utils.featureflags.FeatureFlags;
+import com.artillexstudios.axapi.utils.logging.LogUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -83,6 +84,7 @@ public abstract class AxPlugin extends JavaPlugin {
             public void onPacketReceive(PacketEvent event) {
                 if (event.type() == ServerboundPacketTypes.INTERACT) {
                     if (AxPlugin.this.tracker == null) {
+                        LogUtils.info("No tracker. Package: {}, class: {}", AxPlugin.class.getPackageName(), AxPlugin.class);
                         return;
                     }
 
--- nms/v1_21_R3/src/main/java/com/artillexstudios/axapi/nms/v1_21_R3/wrapper/ServerPlayerWrapper.java
+++ nms/v1_21_R3/src/main/java/com/artillexstudios/axapi/nms/v1_21_R3/wrapper/ServerPlayerWrapper.java
@@ -1,6 +1,7 @@
 package com.artillexstudios.axapi.nms.v1_21_R3.wrapper;
 
-import com.artillexstudios.axapi.nms.v1_21_R3.packet.PacketEncoder;
+import com.artillexstudios.axapi.AxPlugin;
+import com.artillexstudios.axapi.nms.v1_21_R3.packet.ChannelDuplexHandlerPacketListener;
 import com.artillexstudios.axapi.reflection.FieldAccessor;
 import com.artillexstudios.axapi.utils.ComponentSerializer;
 import com.artillexstudios.axapi.utils.PlayerTextures;
@@ -63,7 +64,7 @@ public final class ServerPlayerWrapper implements com.artillexstudios.axapi.nms.
         }
 
         channel.eventLoop().submit(() -> {
-            channel.pipeline().addBefore("encoder", "axapi_encoder", new PacketEncoder("axapi", this.wrapped()));
+            channel.pipeline().addBefore(ServerPlayerWrapper.PACKET_HANDLER, ServerPlayerWrapper.AXAPI_HANDLER, new ChannelDuplexHandlerPacketListener(AxPlugin.getPlugin(AxPlugin.class).flags(), this.wrapped()));
         });
     }
 
