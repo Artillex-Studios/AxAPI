8960f3662ae007003a8f90f0967ad8354513f740
TomTom <93038247+AverageGithub@users.noreply.github.com>
2025-04-20 15:14:06 +0200

    Try a different way to deal with TransformerHandler

--- api/src/main/java/com/artillexstudios/axapi/database/handler/TransformerHandler.java
+++ api/src/main/java/com/artillexstudios/axapi/database/handler/TransformerHandler.java
@@ -1,34 +1,31 @@
 package com.artillexstudios.axapi.database.handler;
 
-import com.artillexstudios.axapi.database.DatabaseHandler;
 import com.artillexstudios.axapi.database.ResultHandler;
-import com.artillexstudios.axapi.utils.Pair;
 
+import java.lang.reflect.InvocationTargetException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Function;
+import java.util.Arrays;
 
 public class TransformerHandler<T> implements ResultHandler<T> {
-    private final Pair<Function<Object, List<Object>>, Function<List<Object>, Object>> transformer;
-    private final DatabaseHandler handler;
     private final Class<T> clazz;
 
-    public TransformerHandler(DatabaseHandler handler, Class<T> clazz) {
-        this.handler = handler;
+    public TransformerHandler(Class<T> clazz) {
         this.clazz = clazz;
-        this.transformer = this.handler.transformer(clazz);
     }
 
     @Override
     public T handle(ResultSet resultSet) throws SQLException {
         int columnCount = resultSet.getMetaData().getColumnCount();
-        List<Object> objects = new ArrayList<>(columnCount);
+        Object[] objects = new Object[columnCount];
         for (int i = 0; i < columnCount; i++) {
-            objects.add(resultSet.getObject(i + 1));
+            objects[i] = resultSet.getObject(i + 1);
         }
 
-        return (T) transformer.second().apply(objects);
+        try {
+            return (T) Arrays.stream(clazz.getDeclaredConstructors()).filter(it -> it.getParameterCount() == columnCount).findFirst().orElseThrow().newInstance(objects);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
