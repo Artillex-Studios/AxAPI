247645b6ab77d2d2729c4105452a9ba3a886867b
TomTom <93038247+AverageGithub@users.noreply.github.com>
2025-04-21 16:43:16 +0200

    Add class debug

--- api/src/main/java/com/artillexstudios/axapi/reflection/ClassUtils.java
+++ api/src/main/java/com/artillexstudios/axapi/reflection/ClassUtils.java
@@ -1,12 +1,15 @@
 package com.artillexstudios.axapi.reflection;
 
 import com.artillexstudios.axapi.utils.logging.LogUtils;
-import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.LoadingCache;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public enum ClassUtils {
     INSTANCE;
@@ -25,15 +28,6 @@ public enum ClassUtils {
         return CLASS_CACHE.get(className) != null;
     }
 
-    public <T> T newInstance(String clazz) {
-        try {
-            return newInstance(Class.forName(clazz));
-        } catch (ClassNotFoundException exception) {
-            LogUtils.error("Could not find class {}!", clazz, exception);
-            throw new RuntimeException(exception);
-        }
-    }
-
     public Class<?> getClass(String clazz) {
         try {
             return Class.forName(clazz);
@@ -43,21 +37,61 @@ public enum ClassUtils {
         }
     }
 
+    public Class<?> getClassOrNull(String clazz) {
+        try {
+            return Class.forName(clazz);
+        } catch (ClassNotFoundException exception) {
+            return null;
+        }
+    }
+
     public boolean isClass(Class<?> clazz, String other) {
         return CLASS_CACHE.get(other) == clazz;
     }
 
-    public Field getDeclaredField(String clazz, String field) {
-        Class<?> cl = getClass(clazz);
-        try {
-            return cl.getDeclaredField(field);
-        } catch (NoSuchFieldException exception) {
-            LogUtils.error("An unexpected error occurred while getting field {} of class {}!", field, clazz, exception);
-            throw new RuntimeException(exception);
-        }
+    public String debugClass(Class<?> clazz) {
+        return this.debugClass(0, clazz);
     }
 
-    public <T> T newInstance(Class<?> clazz) {
-        return null;
+    private String debugClass(int indent, Class<?> clazz) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("--- ").append(clazz.getName()).append(" ---").append(System.lineSeparator());
+        builder.append("\t".repeat(indent)).append("Interface: ").append(clazz.isInterface()).append(System.lineSeparator());
+        builder.append("\t".repeat(indent)).append("Record: ").append(clazz.isRecord()).append(System.lineSeparator());
+        builder.append("\t".repeat(indent)).append("Sealed: ").append(clazz.isSealed()).append(System.lineSeparator());
+        builder.append("\t".repeat(indent)).append("Member: ").append(clazz.isMemberClass()).append(System.lineSeparator());
+        builder.append("\t".repeat(indent)).append("Array: ").append(clazz.isArray()).append(System.lineSeparator());
+        builder.append("\t".repeat(indent)).append("Enum: ").append(clazz.isEnum()).append(System.lineSeparator());
+        builder.append("\t".repeat(indent)).append("Hidden: ").append(clazz.isHidden()).append(System.lineSeparator());
+        builder.append("\t".repeat(indent)).append("Implemented interfaces: ").append(String.join(", ", Arrays.stream(clazz.getInterfaces()).map(java.lang.Class::getName).toList()))
+                .append(System.lineSeparator());
+        builder.append("\t".repeat(indent)).append("Subclasses: ").append(System.lineSeparator());
+        for (Class<?> subClass : clazz.getClasses()) {
+            builder.append("\t".repeat(indent + 1)).append(debugClass(indent + 1, subClass)).append(System.lineSeparator());
+        }
+        builder.append("\t".repeat(indent)).append("Fields: ").append(System.lineSeparator());
+        for (Field declaredField : clazz.getDeclaredFields()) {
+            builder.append("\t".repeat(indent + 1)).append("Name: ").append(declaredField.getName()).append(System.lineSeparator());
+            builder.append("\t".repeat(indent + 1)).append("Modifiers: ").append(Modifier.toString(declaredField.getModifiers())).append(System.lineSeparator());
+            builder.append("\t".repeat(indent + 1)).append("Type: ").append(declaredField.getType()).append(System.lineSeparator());
+            builder.append("\t".repeat(indent + 1)).append("Generic type: ").append(declaredField.getGenericType()).append(System.lineSeparator());
+            builder.append("\t".repeat(indent + 1)).append("Annotated type: ").append(declaredField.getAnnotatedType()).append(System.lineSeparator());
+        }
+
+        Set<Class<?>> superClasses = new HashSet<>();
+        while (true) {
+            clazz = clazz.getSuperclass();
+            if (clazz == null || clazz == Object.class) {
+                break;
+            }
+
+            superClasses.add(clazz);
+        }
+        builder.append("\t".repeat(indent)).append("Super classes: ").append(System.lineSeparator());
+        for (Class<?> superClass : superClasses) {
+            builder.append("\t".repeat(indent + 1)).append(debugClass(indent + 1, superClass)).append(System.lineSeparator());
+        }
+
+        return builder.toString();
     }
 }
