8db9054557d600982d065e75907bce1a5639a16c
TomTom <93038247+AverageGithub@users.noreply.github.com>
2025-06-18 15:00:28 +0200

    Start 1.21.6 work

---
+++ nms/v1_21_R5/src/main/java/com/artillexstudios/axapi/nms/v1_21_R5/packet/FriendlyByteBufWrapper.java
@@ -0,0 +1,286 @@
+package com.artillexstudios.axapi.nms.v1_21_R5.packet;
+
+import com.artillexstudios.axapi.nms.v1_21_R5.items.WrappedItemStack;
+import com.artillexstudios.axapi.nms.v1_21_R5.items.nbt.CompoundTag;
+import com.artillexstudios.axapi.packet.FriendlyByteBuf;
+import com.artillexstudios.axapi.utils.ComponentSerializer;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.text.Component;
+import net.minecraft.core.component.DataComponentExactPredicate;
+import net.minecraft.core.component.DataComponentMap;
+import net.minecraft.core.component.PatchedDataComponentMap;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.chat.ComponentSerialization;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.trading.ItemCost;
+import net.minecraft.world.level.block.Block;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.craftbukkit.block.data.CraftBlockData;
+
+import java.util.Optional;
+
+public record FriendlyByteBufWrapper(RegistryFriendlyByteBuf buf) implements FriendlyByteBuf {
+
+    @Override
+    public WrappedItemStack readItemStack(com.artillexstudios.axapi.items.WrappedItemStack.CodecData codecData) {
+        return switch (codecData) {
+            case OPTIONAL_UNTRUSTED_STREAM_CODEC ->
+                    new WrappedItemStack(ItemStack.OPTIONAL_UNTRUSTED_STREAM_CODEC.decode(this.buf));
+            case OPTIONAL_STREAM_CODEC -> new WrappedItemStack(ItemStack.OPTIONAL_STREAM_CODEC.decode(this.buf));
+            case STREAM_CODEC -> new WrappedItemStack(ItemStack.STREAM_CODEC.decode(this.buf));
+        };
+    }
+
+    @Override
+    public void writeItemStack(com.artillexstudios.axapi.items.WrappedItemStack wrappedItemStack, WrappedItemStack.CodecData codecData) {
+        switch (codecData) {
+            case OPTIONAL_UNTRUSTED_STREAM_CODEC ->
+                    ItemStack.OPTIONAL_UNTRUSTED_STREAM_CODEC.encode(this.buf, ((WrappedItemStack) wrappedItemStack).asMinecraft());
+            case OPTIONAL_STREAM_CODEC ->
+                    ItemStack.OPTIONAL_STREAM_CODEC.encode(this.buf, ((WrappedItemStack) wrappedItemStack).asMinecraft());
+            case STREAM_CODEC ->
+                    ItemStack.STREAM_CODEC.encode(this.buf, ((WrappedItemStack) wrappedItemStack).asMinecraft());
+        }
+    }
+
+    @Override
+    public byte[] readByteArray() {
+        return this.buf.readByteArray();
+    }
+
+    @Override
+    public byte[] readByteArray(int maxSize) {
+        return this.buf.readByteArray(maxSize);
+    }
+
+    @Override
+    public void writeByteArray(byte[] bytes) {
+        this.buf.writeByteArray(bytes);
+    }
+
+    @Override
+    public Component readComponent() {
+        return ComponentSerializer.INSTANCE.fromVanilla(ComponentSerialization.TRUSTED_STREAM_CODEC.decode(this.buf));
+    }
+
+    @Override
+    public void writeComponent(Component component) {
+        ComponentSerialization.TRUSTED_STREAM_CODEC.encode(buf, ComponentSerializer.INSTANCE.toVanilla(component));
+    }
+
+    @Override
+    public int readVarInt() {
+        return this.buf.readVarInt();
+    }
+
+    @Override
+    public void writeVarInt(int varInt) {
+        this.buf.writeVarInt(varInt);
+    }
+
+    @Override
+    public com.artillexstudios.axapi.items.nbt.CompoundTag readNBT() {
+        return new CompoundTag(this.buf.readNbt());
+    }
+
+    @Override
+    public void writeNBT(com.artillexstudios.axapi.items.nbt.CompoundTag tag) {
+        this.buf.writeNbt((net.minecraft.nbt.CompoundTag) tag.getParent());
+    }
+
+    @Override
+    public String readUTF() {
+        return this.buf.readUtf();
+    }
+
+    @Override
+    public String readUTF(int length) {
+        return this.buf.readUtf(length);
+    }
+
+    @Override
+    public void writeUTF(String utf) {
+        this.buf.writeUtf(utf);
+    }
+
+    @Override
+    public Key readResourceLocation() {
+        ResourceLocation resourceLocation = this.buf.readResourceLocation();
+        return Key.key(resourceLocation.getNamespace(), resourceLocation.getPath());
+    }
+
+    @Override
+    public void writeResourceLocation(Key key) {
+        this.buf.writeResourceLocation(ResourceLocation.tryBuild(key.namespace(), key.value()));
+    }
+
+    @Override
+    public void writeFloat(float value) {
+        this.buf.writeFloat(value);
+    }
+
+    @Override
+    public float readFloat() {
+        return this.buf.readFloat();
+    }
+
+    @Override
+    public void writeShort(int value) {
+        this.buf.writeShort(value);
+    }
+
+    @Override
+    public short readShort() {
+        return this.buf.readShort();
+    }
+
+    @Override
+    public void writeDouble(double value) {
+        this.buf.writeDouble(value);
+    }
+
+    @Override
+    public double readDouble() {
+        return this.buf.readDouble();
+    }
+
+    @Override
+    public void writeBoolean(boolean value) {
+        this.buf.writeBoolean(value);
+    }
+
+    @Override
+    public boolean readBoolean() {
+        return this.buf.readBoolean();
+    }
+
+    @Override
+    public void writeInt(int value) {
+        this.buf.writeInt(value);
+    }
+
+    @Override
+    public int readInt() {
+        return this.buf.readInt();
+    }
+
+    @Override
+    public void writeVarLong(long value) {
+        this.buf.writeVarLong(value);
+    }
+
+    @Override
+    public long readVarLong() {
+        return this.buf.readVarLong();
+    }
+
+    @Override
+    public void writeLong(long value) {
+        this.buf.writeLong(value);
+    }
+
+    @Override
+    public long readLong() {
+        return this.buf.readLong();
+    }
+
+    @Override
+    public void writeByte(int value) {
+        this.buf.writeByte(value);
+    }
+
+    @Override
+    public byte readByte() {
+        return this.buf.readByte();
+    }
+
+    @Override
+    public short readUnsignedByte() {
+        return this.buf.readUnsignedByte();
+    }
+
+    @Override
+    public void readerIndex(int index) {
+        this.buf.readerIndex(index);
+    }
+
+    @Override
+    public int readerIndex() {
+        return this.buf.readerIndex();
+    }
+
+    @Override
+    public void writerIndex(int index) {
+        this.buf.writerIndex(index);
+    }
+
+    @Override
+    public int writerIndex() {
+        return this.buf.writerIndex();
+    }
+
+    @Override
+    public WrappedItemStack readItemCost() {
+        return new WrappedItemStack(ItemCost.STREAM_CODEC.decode(this.buf).itemStack());
+    }
+
+    @Override
+    public void writeItemCost(com.artillexstudios.axapi.items.WrappedItemStack itemCost) {
+        ItemStack nmsItem = ((WrappedItemStack) itemCost).asMinecraft();
+        ItemCost.STREAM_CODEC.encode(this.buf, new ItemCost(nmsItem.getItemHolder(), nmsItem.getCount(), DataComponentExactPredicate.allOf(PatchedDataComponentMap.fromPatch(DataComponentMap.EMPTY, nmsItem.getComponentsPatch()))));
+    }
+
+    @Override
+    public Optional<com.artillexstudios.axapi.items.WrappedItemStack> readOptionalItemCost() {
+        return ItemCost.OPTIONAL_STREAM_CODEC.decode(this.buf).map(cost -> new WrappedItemStack(cost.itemStack()));
+    }
+
+    @Override
+    public void writeOptionalItemCost(Optional<com.artillexstudios.axapi.items.WrappedItemStack> itemCost) {
+        ItemCost.OPTIONAL_STREAM_CODEC.encode(this.buf, itemCost.map(stack -> {
+            ItemStack nmsItem = ((WrappedItemStack) stack).itemStack;
+            return new ItemCost(nmsItem.getItemHolder(), nmsItem.getCount(), DataComponentExactPredicate.allOf(PatchedDataComponentMap.fromPatch(DataComponentMap.EMPTY, nmsItem.getComponentsPatch())));
+        }));
+    }
+
+    @Override
+    public int readUnsignedShort() {
+        return this.buf.readUnsignedShort();
+    }
+
+    @Override
+    public void writeBlockData(BlockData blockData) {
+        this.buf.writeVarInt(Block.getId(((CraftBlockData) blockData).getState()));
+    }
+
+    @Override
+    public BlockData readBlockData() {
+        return CraftBlockData.fromData(Block.BLOCK_STATE_REGISTRY.byId(this.readVarInt()));
+    }
+
+    @Override
+    public void writeBytes(FriendlyByteBuf buf) {
+        this.buf.writeBytes(((FriendlyByteBufWrapper) buf).buf());
+    }
+
+    @Override
+    public FriendlyByteBuf readBytes(int length) {
+        return PacketTransformer.wrap(this.buf.readBytes(length));
+    }
+
+    @Override
+    public FriendlyByteBuf copy() {
+        return PacketTransformer.copy(this);
+    }
+
+    @Override
+    public int readableBytes() {
+        return this.buf.readableBytes();
+    }
+
+    @Override
+    public void release() {
+        this.buf.release();
+    }
+}
---
+++ nms/v1_21_R5/src/main/java/com/artillexstudios/axapi/nms/v1_21_R5/wrapper/ServerPlayerWrapper.java
@@ -0,0 +1,194 @@
+package com.artillexstudios.axapi.nms.v1_21_R5.wrapper;
+
+import com.artillexstudios.axapi.nms.v1_21_R5.packet.ChannelDuplexHandlerPacketListener;
+import com.artillexstudios.axapi.nms.v1_21_R5.packet.PacketTransformer;
+import com.artillexstudios.axapi.packet.wrapper.PacketWrapper;
+import com.artillexstudios.axapi.reflection.FieldAccessor;
+import com.artillexstudios.axapi.utils.ComponentSerializer;
+import com.artillexstudios.axapi.utils.PlayerTextures;
+import com.artillexstudios.axapi.utils.logging.LogUtils;
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import io.netty.channel.Channel;
+import net.kyori.adventure.text.Component;
+import net.minecraft.network.Connection;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientboundSystemChatPacket;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.ai.attributes.AttributeMap;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.craftbukkit.attribute.CraftAttribute;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public final class ServerPlayerWrapper implements com.artillexstudios.axapi.nms.wrapper.ServerPlayerWrapper {
+    private static final FieldAccessor connectionAccessor = FieldAccessor.builder()
+            .withClass("net.minecraft.server.network.ServerCommonPacketListenerImpl")
+            .withField("e")
+            .build();
+    private static final FieldAccessor channelAccessor = FieldAccessor.builder()
+            .withClass("net.minecraft.network.NetworkManager")
+            .withField("n")
+            .build();
+    private static final FieldAccessor attributeAccessor = FieldAccessor.builder()
+            .withClass(AttributeMap.class)
+            .withField("d")
+            .build();
+    private final PacketTransformer transformer;
+    private Player wrapped;
+    private ServerPlayer serverPlayer;
+
+    public ServerPlayerWrapper(Player player) {
+        this.wrapped = player;
+        this.transformer = new PacketTransformer(this);
+    }
+
+    public ServerPlayerWrapper(ServerPlayer player) {
+        this.serverPlayer = player;
+        this.transformer = new PacketTransformer(this);
+    }
+
+    @Override
+    public void inject() {
+        this.update();
+
+        Connection connection = connectionAccessor.get(this.serverPlayer.connection, Connection.class);
+        Channel channel = channelAccessor.get(connection, Channel.class);
+
+        if (!channel.pipeline().names().contains(ServerPlayerWrapper.PACKET_HANDLER)) {
+            return;
+        }
+
+        if (channel.pipeline().names().contains(ServerPlayerWrapper.AXAPI_HANDLER)) {
+            return;
+        }
+
+        channel.eventLoop().submit(() -> {
+            channel.pipeline().addBefore(ServerPlayerWrapper.PACKET_HANDLER, ServerPlayerWrapper.AXAPI_HANDLER, new ChannelDuplexHandlerPacketListener(this));
+        });
+    }
+
+    @Override
+    public void uninject() {
+        this.update();
+
+        Connection connection = connectionAccessor.get(this.serverPlayer.connection, Connection.class);
+        Channel channel = channelAccessor.get(connection, Channel.class);
+
+        channel.eventLoop().submit(() -> {
+            if (channel.pipeline().get(ServerPlayerWrapper.AXAPI_HANDLER) != null) {
+                channel.pipeline().remove(ServerPlayerWrapper.AXAPI_HANDLER);
+            }
+        });
+    }
+
+    @Override
+    public void sendPacket(Object packet) {
+        this.update();
+
+        if (packet instanceof PacketWrapper wrapper) {
+            this.serverPlayer.connection.send((Packet<?>) wrapper.cached());
+            return;
+        }
+
+        if (!(packet instanceof Packet<?> p)) {
+            LogUtils.warn("Failed to send unknown packet to player {}! Packet: {}", this.wrapped().getName(), packet);
+            return;
+        }
+
+        this.serverPlayer.connection.send(p);
+    }
+
+    @Override
+    public void message(Component message) {
+        this.update();
+        this.serverPlayer.connection.send(new ClientboundSystemChatPacket((net.minecraft.network.chat.Component) ComponentSerializer.INSTANCE.toVanilla(message), false));
+    }
+
+    @Override
+    public double getBase(Attribute attribute) {
+        this.update();
+
+        AttributeMap map = this.serverPlayer.getAttributes();
+        AttributeSupplier supplier = attributeAccessor.get(map, AttributeSupplier.class);
+        return supplier.getBaseValue(CraftAttribute.bukkitToMinecraftHolder(attribute));
+    }
+
+    @Override
+    public PlayerTextures textures() {
+        this.update();
+        GameProfile profile = this.serverPlayer.getGameProfile();
+        Optional<Property> property = profile.getProperties()
+                .get("textures")
+                .stream()
+                .findFirst();
+
+        if (property.isEmpty()) {
+            return new PlayerTextures(null, null);
+        }
+
+        Property value = property.get();
+        return new PlayerTextures(value.value(), value.signature());
+    }
+
+    @Override
+    public double getX() {
+        this.update();
+        return this.serverPlayer.getX();
+    }
+
+    @Override
+    public double getZ() {
+        this.update();
+        return this.serverPlayer.getZ();
+    }
+
+    @Override
+    public Player wrapped() {
+        Player wrapped = this.wrapped;
+        if (wrapped == null) {
+            wrapped = this.serverPlayer.getBukkitEntity();
+            this.wrapped = wrapped;
+        }
+
+        return wrapped;
+    }
+
+    @Override
+    public void update(boolean force) {
+        if (this.serverPlayer == null || force) {
+            this.serverPlayer = ((CraftPlayer) this.wrapped).getHandle();
+        }
+    }
+
+    @Override
+    public ServerPlayer asMinecraft() {
+        this.update();
+        return this.serverPlayer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof ServerPlayerWrapper that)) {
+            return false;
+        }
+
+        this.update();
+        that.update();
+        if (Objects.equals(this.serverPlayer, that.serverPlayer)) {
+            return true;
+        }
+
+        return this.wrapped().getUniqueId().equals(that.wrapped().getUniqueId());
+    }
+
+    @Override
+    public int hashCode() {
+        this.update();
+        return Objects.hashCode(this.serverPlayer);
+    }
+}
