19d158e00a21b51fbf30826b06804f4e75c438b2
TomTom <93038247+AverageGithub@users.noreply.github.com>
2025-06-26 19:49:27 +0200

    Fix a compile error

--- api/src/main/java/com/artillexstudios/axapi/AxPlugin.java
+++ api/src/main/java/com/artillexstudios/axapi/AxPlugin.java
@@ -93,7 +93,7 @@ public abstract class AxPlugin extends JavaPlugin {
                         return;
                     }
 
-                    ClientboundSetPassengersWrapper passengersWrapper = new ClientboundSetPassengersWrapper(entityId, List.of(rider.id()));
+                    ClientboundSetPassengersWrapper passengersWrapper = new ClientboundSetPassengersWrapper(entityId, new int[]{rider.id()});
                     ServerPlayerWrapper serverPlayerWrapper = ServerPlayerWrapper.wrap(event.player());
                     serverPlayerWrapper.sendPacket(passengersWrapper);
                 }
