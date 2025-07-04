207fe719526854fcc6b4b156d4826fe88b76ec93
TomTom <93038247+AverageGithub@users.noreply.github.com>
2025-07-04 16:47:16 +0200

    Added a simple pattern matcher

---
+++ api/src/main/java/com/artillexstudios/axapi/utils/SimplePattern.java
@@ -0,0 +1,41 @@
+package com.artillexstudios.axapi.utils;
+
+public class SimplePattern {
+    private final String expression;
+    private final boolean beginMatch;
+    private final boolean endMatch;
+
+    public SimplePattern(String expression) {
+        String modifiableExpression = expression;
+        if (expression.charAt(0) == '%') {
+            this.beginMatch = true;
+            modifiableExpression = modifiableExpression.substring(1);
+        } else {
+            this.beginMatch = false;
+        }
+
+        if (expression.charAt(expression.length() - 1) == '%') {
+            this.endMatch = true;
+            modifiableExpression = modifiableExpression.substring(0, modifiableExpression.length() - 1);
+        } else {
+            this.endMatch = false;
+        }
+        this.expression = modifiableExpression;
+    }
+
+    public boolean matches(String other) {
+        if (this.beginMatch && this.endMatch) {
+            return other.contains(this.expression);
+        }
+
+        if (this.beginMatch) {
+            return other.endsWith(this.expression);
+        }
+
+        if (this.endMatch) {
+            return other.startsWith(this.expression);
+        }
+
+        return other.equals(this.expression);
+    }
+}
