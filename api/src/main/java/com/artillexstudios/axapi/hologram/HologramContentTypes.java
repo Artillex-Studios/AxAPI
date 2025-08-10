e22dd56cceb322e94b6ea74d5a9a62fef30acf7c
TomTom <93038247+AverageGithub@users.noreply.github.com>
2025-08-10 13:58:36 +0200

    Work on refactoring the Hologram system

--- api/src/main/java/com/artillexstudios/axapi/hologram/HologramContentTypes.java
+++ api/src/main/java/com/artillexstudios/axapi/hologram/HologramContentTypes.java
@@ -5,8 +5,8 @@ import net.kyori.adventure.text.Component;
 import org.bukkit.entity.EntityType;
 
 public final class HologramContentTypes {
-    public static final HologramContentType<Component> TEXT = new HologramContentType<>();
-    public static final HologramContentType<EntityType> ENTITY = new HologramContentType<>();
-    public static final HologramContentType<WrappedItemStack> ITEM_STACK = new HologramContentType<>();
-    public static final HologramContentType<WrappedItemStack> HEAD = new HologramContentType<>();
+//    public static final HologramContentType<Component> TEXT = new HologramContentType<>();
+//    public static final HologramContentType<EntityType> ENTITY = new HologramContentType<>();
+//    public static final HologramContentType<WrappedItemStack> ITEM_STACK = new HologramContentType<>();
+//    public static final HologramContentType<WrappedItemStack> HEAD = new HologramContentType<>();
 }
