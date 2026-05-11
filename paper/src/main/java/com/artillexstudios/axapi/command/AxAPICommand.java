5dddb1d0dfc904fede71d2daaf61fcf228b2f89a
AverageGithub <93038247+AverageGithub@users.noreply.github.com>
2026-05-11 16:33:40 +0200

    Add debug to snbt copying

--- paper/src/main/java/com/artillexstudios/axapi/command/AxAPICommand.java
+++ paper/src/main/java/com/artillexstudios/axapi/command/AxAPICommand.java
@@ -258,6 +258,7 @@ public final class AxAPICommand {
 
         String serialized = (String) builder.serialize(true).get("snbt");
         String escaped = MiniMessage.miniMessage().escapeTags(serialized);
+        System.out.println("Escaped " + escaped);
         Component message = MiniMessage.miniMessage().deserialize(PREFIX + "<click:copy_to_clipboard:" + escaped + "><green>Dumped data! Click to copy to clipboard: " + escaped + "</click>");
         System.out.println(message);
         ServerPlayerWrapper wrapper = ServerPlayerWrapper.wrap(player);
