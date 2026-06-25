accd58b8c5c10f2d5b516d3751987ced9398dfdb
AverageGithub <93038247+AverageGithub@users.noreply.github.com>
2026-06-25 11:42:12 +0200

    Work on a library downloading system

--- api/src/main/java/com/artillexstudios/axapi/libraries/LibraryCache.java
+++ api/src/main/java/com/artillexstudios/axapi/libraries/LibraryCache.java
@@ -20,6 +20,37 @@ public final class LibraryCache {
         this.configuration.load();
     }
 
+    public boolean cache(Library library) {
+        boolean contains = CacheConfiguration.libraries.contains(library);
+        if (!contains) {
+            CacheConfiguration.libraries.add(library);
+            this.save();
+        }
+        return contains;
+    }
+
+    public boolean cache(Repository repository) {
+        boolean contains = CacheConfiguration.repositories.contains(repository);
+        if (!contains) {
+            CacheConfiguration.repositories.add(repository);
+            this.save();
+        }
+        return contains;
+    }
+
+    public boolean cache(Relocation relocation) {
+        boolean contains = CacheConfiguration.relocations.contains(relocation);
+        if (!contains) {
+            CacheConfiguration.relocations.add(relocation);
+            this.save();
+        }
+        return contains;
+    }
+
+    public void save() {
+        this.configuration.save();
+    }
+
     public boolean isCacheCreated() {
         return this.isCacheCreated;
     }
--- api/src/main/java/com/artillexstudios/axapi/libraries/LibraryDownloader.java
+++ api/src/main/java/com/artillexstudios/axapi/libraries/LibraryDownloader.java
@@ -13,6 +13,7 @@ public final class LibraryDownloader {
     private final List<Library> libraries = new ArrayList<>();
     private final List<Repository> repositories = new ArrayList<>();
     private final List<Relocation> relocations = new ArrayList<>();
+    private final List<Path> libraryPaths = new ArrayList<>();
     private final Path librariesFolder;
     private final boolean cleanIfNoCache;
 
@@ -25,23 +26,43 @@ public final class LibraryDownloader {
         this.cleanIfNoCache = cleanIfNoCache;
         this.libraryCache = new LibraryCache(this.librariesFolder);
         if (this.cleanIfNoCache && !this.libraryCache.isCacheCreated()) {
-            try {
-                Files.delete(librariesFolder);
-            } catch (IOException exception) {
-                LogUtils.error("An exception occurred while downloading libraries!", exception);
-            }
+            this.cleanCache(librariesFolder);
+        }
+        this.libraryCache.load();
+    }
+
+    private void cleanCache(Path librariesFolder) {
+        try {
+            Files.delete(librariesFolder);
+        } catch (IOException exception) {
+            LogUtils.error("An exception occurred while downloading libraries!", exception);
         }
     }
 
     public void addLibrary(Library library) {
         this.libraries.add(library);
+        if (this.libraryCache.cache(library)) {
+            // We already have the library cached
+            return;
+        }
+
     }
 
     public void addRepository(Repository repository) {
         this.repositories.add(repository);
+        if (this.libraryCache.cache(repository)) {
+            return;
+        }
+
     }
 
     public void addRelocation(Relocation relocation) {
         this.relocations.add(relocation);
+        if (this.libraryCache.cache(relocation)) {
+            return;
+        }
+        // We need to remove all cached jars and relocate them
+        this.cleanCache(this.librariesFolder);
+        this.libraryCache.save();
     }
 }
