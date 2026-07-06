cc805151e5e2429a5c2cfa59f8be22f55828bf64
AverageGithub <93038247+AverageGithub@users.noreply.github.com>
2026-07-06 12:19:10 +0200

    More utility methods

--- paper/src/main/java/com/artillexstudios/axapi/utils/ContainerUtils.java
+++ paper/src/main/java/com/artillexstudios/axapi/utils/ContainerUtils.java
@@ -2,6 +2,7 @@ package com.artillexstudios.axapi.utils;
 
 import com.artillexstudios.axapi.scheduler.Scheduler;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
@@ -21,5 +22,25 @@ public enum ContainerUtils {
                 });
             }
         });
+    }
+
+    public void addOrDrop(Inventory inventory, ItemStack item, Location location) {
+        this.addOrDrop(inventory, List.of(item), location);
+    }
+
+    public void addOrDrop(Player player, List<ItemStack> stacks) {
+        Scheduler.get().run(player, task -> {
+            Location location = player.getLocation();
+            for (ItemStack stack : stacks) {
+                HashMap<Integer, ItemStack> remaining = player.getInventory().addItem(stack);
+                remaining.forEach((k, v) -> {
+                    location.getWorld().dropItem(location, v);
+                });
+            }
+        }, () -> {});
+    }
+
+    public void addOrDrop(Player player, ItemStack stack) {
+        this.addOrDrop(player, List.of(stack));
     }
 }
