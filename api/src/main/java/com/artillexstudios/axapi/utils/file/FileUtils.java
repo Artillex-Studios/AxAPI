2eea3036abc54acf7bf5273c1509e419bf7d65be
AverageGithub <93038247+AverageGithub@users.noreply.github.com>
2026-08-06 14:07:28 +0200

    Add copying directory functionality

--- api/src/main/java/com/artillexstudios/axapi/utils/file/FileUtils.java
+++ api/src/main/java/com/artillexstudios/axapi/utils/file/FileUtils.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.stream.Stream;
 
 public interface FileUtils {
@@ -35,6 +36,37 @@ public interface FileUtils {
             if (FeatureFlags.DEBUG.get()) {
                 LogUtils.error("Failed to delete directory {}", directory);
             }
+        }
+    }
+
+    static void copyDirectory(Path from, Path to) {
+        try {
+            if (!Files.isDirectory(from)) {
+                if (!Files.exists(to.getParent())) {
+                    Files.createDirectories(to.getParent());
+                }
+                Files.copy(from, to, StandardCopyOption.COPY_ATTRIBUTES, StandardCopyOption.REPLACE_EXISTING);
+                return;
+            }
+
+            try (Stream<Path> paths = Files.list(from)) {
+                paths.forEach(path -> {
+                    Path newTo = to.resolve(from.relativize(path));
+                    if (Files.isDirectory(path)) {
+                        try {
+                            if (!Files.exists(newTo)) {
+                                Files.createDirectory(newTo);
+                            }
+                        } catch (IOException exception) {
+                            LogUtils.error("Failed to copy directory from path {} to path {}!", path, newTo);
+                        }
+                    }
+
+                    copyDirectory(path, newTo);
+                });
+            }
+        } catch (IOException exception) {
+            LogUtils.error("Failed to copy directory from path {} to path {}!", from, to);
         }
     }
 }
